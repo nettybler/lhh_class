ad167df1c8f51c74afd122b2f29f40832acd169a
ingrid <ingrid@phariss.com>
2018-11-07 09:27:19 -0800

    One week's work.

--- cardClicked.js
+++ cardClicked.js
@@ -1,3 +1,44 @@
 function cardClicked(card, column, lastCard, lastColumn) {
-    alert("You clicked " + card.name() + " in column " + column.id);
+    if (lastCard == null) {
+        card.selected = true;
+        this.remember(card, column);
+    }
+    else {
+        alert(lastCard.name() + 'moving');
+     lastCard.selected=false;
+        this.forget();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
